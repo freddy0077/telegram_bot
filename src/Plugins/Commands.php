137f6c4fe0cdbf0e308f8ad8bff1caa636648d95
Frederick <frederickankamah988@gmail.com>
2023-07-31 15:19:26 +0000

    menu pages

--- src/Plugins/Commands.php
+++ src/Plugins/Commands.php
@@ -6,70 +6,41 @@ use TelegramBot\Entities\InlineKeyboard;
 use TelegramBot\Entities\InlineKeyboardButton;
 use TelegramBot\Entities\KeyboardButton;
 use TelegramBot\Entities\Message;
+use TelegramBot\Entities\Keyboard;
 use TelegramBot\Enums\ParseMode;
 use TelegramBot\Request;
 
 class Commands extends \TelegramBot\Plugin
 {
-
     public function onMessage(int $update_id, Message $message): \Generator
     {
-        $chatStates = [];
-//        if ($message->getText() == '/start') {
-//
-//            // Sending the message with the initial buttons
-//            yield Request::sendMessage([
-//                'chat_id' => $message->getChat()->getId(),
-//                'parse_mode' => ParseMode::MARKDOWN,
-//                'text' => "Please tap the button below to choose options!",
-//                'reply_markup' => InlineKeyboard::make()->setKeyboard([
-//                    [InlineKeyboardButton::make('🟢 Play for free')->setCallbackData('free_play')],
-//                    [InlineKeyboardButton::make('🔴 Play with cash')->setCallbackData('show_cash_games')]
-//                ])
-//            ]);
-//        }
+
+        $contact = $message->getContact();
 
         if ($message->getText() == '/start') {
-
-            // Step 1: Ask for phone number
+            // Request phone number
             yield Request::sendMessage([
                 'chat_id' => $message->getChat()->getId(),
-                'text' => "Please share your phone number:",
-                'reply_markup' => InlineKeyboard::make()
+                'text' => "Please share your phone number to get started!",
+                'reply_markup' => Keyboard::make()
+                    ->setResizeKeyboard(true)
+                    ->setOneTimeKeyboard(true)
                     ->setKeyboard([
-                        [KeyboardButton::make('📱 Share Phone Number')->setRequestContact(true)],
+                        [KeyboardButton::make('Share Phone Number')->setRequestContact(true)]
                     ])
             ]);
-
-            $chatStates[$message->getChat()->getId()] = 'waiting_for_phone';
-
-        } elseif ($message->getContact() && $message->getContact()->getPhoneNumber()) {
-
-            // Assuming that after sharing the phone number, you store it somewhere, e.g., in a database.
-            // You can then proceed to Step 2: Ask for network details
-            yield Request::sendMessage([
-                'chat_id' => $message->getChat()->getId(),
-                'text' => "Please enter your network details:",
-            ]);
-
-            $chatStates[$message->getChat()->getId()] = 'waiting_for_network';
-
-        } elseif (isset($chatStates[$message->getChat()->getId()]) && $chatStates[$message->getChat()->getId()] == 'waiting_for_network') {
-
-            // Save the provided network details and then proceed to Step 3: Display the buttons
+        } elseif ($contact && $contact->getPhoneNumber()) {
+            $phoneNumber = $contact->getPhoneNumber();
             yield Request::sendMessage([
                 'chat_id' => $message->getChat()->getId(),
                 'parse_mode' => ParseMode::MARKDOWN,
-                'text' => "Please tap the button below to choose options!",
+                'text' => "Thanks for sharing your number. Please tap the button below to choose options!",
                 'reply_markup' => InlineKeyboard::make()->setKeyboard([
                     [InlineKeyboardButton::make('🟢 Play for free')->setCallbackData('free_play')],
                     [InlineKeyboardButton::make('🔴 Play with cash')->setCallbackData('show_cash_games')]
                 ])
             ]);
-            unset($chatStates[$message->getChat()->getId()]);
         }
-
-
 
         if ($message->getText() == '/help') {
             yield Request::sendMessage([
@@ -81,7 +52,5 @@ class Commands extends \TelegramBot\Plugin
                     "/help - Show this help page"
             ]);
         }
-
-
     }
 }
