7d84be70b2cbdb786bce7446c7e54c4a6b7029e1
Frederick <frederickankamah988@gmail.com>
2023-07-31 21:01:13 +0000

    menu pages

---
+++ public/includes/perm-html.php
@@ -0,0 +1,128 @@
+<section class="cafe-page cafe-items">
+	<div class="cafe-item js-item" data-item-id="1" data-item-price="4990">
+		<div class="cafe-item-counter js-item-counter">1</div>
+		<div class="cafe-item-photo">
+			<picture class="cafe-item-lottie js-item-lottie">
+				<source type="application/x-tgsticker" srcset="./img/tgs/Burger.tgs">
+				<img src="./img/rsz_lottery-circle.png"
+				     alt="Burger">
+			</picture>
+		</div>
+		<div class="cafe-item-label">
+			<span class="cafe-item-title">Perm 2</span>
+			<span class="cafe-item-price"></span>
+		</div>
+		<div class="cafe-item-buttons">
+			<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+			<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">
+				<span class="button-item-label">Play</span>
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+		</div>
+	</div>
+	<div class="cafe-item js-item" data-item-id="2" data-item-price="1490">
+		<div class="cafe-item-counter js-item-counter">1</div>
+		<div class="cafe-item-photo">
+			<picture class="cafe-item-lottie js-item-lottie">
+				<source type="application/x-tgsticker" srcset="./img/tgs/Fries.tgs">
+				<img src="./img/rsz_lottery-circle.png"
+				     alt="Fries">
+			</picture>
+		</div>
+		<div class="cafe-item-label">
+			<span class="cafe-item-title">Perm 3</span>
+			<span class="cafe-item-price"></span>
+		</div>
+		<div class="cafe-item-buttons">
+			<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+			<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">
+				<span class="button-item-label">Play</span>
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+		</div>
+	</div>
+
+	<div class="cafe-item js-item" data-item-id="3" data-item-price="3490">
+		<div class="cafe-item-counter js-item-counter">1</div>
+		<div class="cafe-item-photo">
+			<picture class="cafe-item-lottie js-item-lottie">
+				<source type="application/x-tgsticker" srcset="./img/tgs/Hotdog.tgs">
+				<img src="./img/rsz_lottery-circle.png"
+
+				     alt="Hotdog">
+			</picture>
+		</div>
+		<div class="cafe-item-label">
+			<span class="cafe-item-title">Perm 4</span>
+			<span class="cafe-item-price"></span>
+		</div>
+		<div class="cafe-item-buttons">
+			<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+			<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">
+				<span class="button-item-label">Play</span>
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+		</div>
+	</div>
+
+	<div class="cafe-item js-item" data-item-id="3" data-item-price="3490">
+		<div class="cafe-item-counter js-item-counter">1</div>
+		<div class="cafe-item-photo">
+			<picture class="cafe-item-lottie js-item-lottie">
+				<source type="application/x-tgsticker" srcset="./img/tgs/Hotdog.tgs">
+				<img src="./img/rsz_lottery-circle.png"
+
+				     alt="Hotdog">
+			</picture>
+		</div>
+		<div class="cafe-item-label">
+			<span class="cafe-item-title">Perm 5</span>
+			<span class="cafe-item-price"></span>
+		</div>
+		<div class="cafe-item-buttons">
+			<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+			<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">
+				<span class="button-item-label">Play</span>
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+		</div>
+	</div>
+
+	<div class="cafe-item js-item" data-item-id="3" data-item-price="3490">
+		<div class="cafe-item-counter js-item-counter">1</div>
+		<div class="cafe-item-photo">
+			<picture class="cafe-item-lottie js-item-lottie">
+				<source type="application/x-tgsticker" srcset="./img/tgs/Hotdog.tgs">
+				<img src="./img/rsz_lottery-circle.png"
+
+				     alt="Hotdog">
+			</picture>
+		</div>
+		<div class="cafe-item-label">
+			<span class="cafe-item-title">Perm 6</span>
+			<span class="cafe-item-price"></span>
+		</div>
+		<div class="cafe-item-buttons">
+			<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+			<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">
+				<span class="button-item-label">Play</span>
+				<span class="ripple-mask"><span class="ripple"></span></span>
+			</button>
+		</div>
+	</div>
+
+	<div class="cafe-item-shadow"></div>
+	<div class="cafe-item-shadow"></div>
+	<div class="cafe-item-shadow"></div>
+	<div class="cafe-item-shadow"></div>
+</section>
