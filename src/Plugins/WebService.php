ccdb117eebb96ccf1df9ec74685aa3d7aab0fc9b
Frederick <frederickankamah988@gmail.com>
2023-08-03 15:19:50 +0000

    new changes

--- src/Plugins/WebService.php
+++ src/Plugins/WebService.php
@@ -38,23 +38,73 @@ class WebService extends \TelegramBot\Plugin
         "perm6" => 6,
     ];
 
-    /**
-     * @param WebAppData $webAppData
-     * @return \Generator
-     */
     public function onWebAppData(WebAppData $webAppData): \Generator
     {
+        header('Content-Type: application/json');
 
+        $rawData = $webAppData->getRawData();
+        $method = $rawData['method'] ?? '';
+
+        switch ($method) {
+            case 'makeOrder':
+                yield $this->handleMakeOrder($webAppData);
+                break;
+
+            case 'checkInitData':
+                Response::send(StatusCode::OK);
+                break;
+
+            case 'sendMessage':
+                yield $this->handleSendMessage($webAppData);
+                break;
+
+            default:
+                yield $this->handleRequestByDataType($webAppData);
+        }
+    }
+
+    private function handleMakeOrder(WebAppData $webAppData): \Generator
+    {
+        yield Request::sendMessage([
+            'chat_id' => $webAppData->getUser()->getId(),
+            'parse_mode' => ParseMode::MARKDOWN,
+            'text' => "Your order has been placed successfully! 🍟" . "\n\n" .
+                "Your order is: \n`" . $this->parseOrder($webAppData->getRawData()['order_data']) . "`" . "\n" .
+                "Your order will be delivered to you in 30 minutes. 🚚",
+        ]);
+
+        Response::send(StatusCode::OK);
+    }
+
+    private function handleSendMessage(WebAppData $webAppData): \Generator
+    {
+        yield Request::sendMessage([
+            'chat_id' => $webAppData->getUser()->getId(),
+            'parse_mode' => ParseMode::MARKDOWN,
+            'text' => "Hello World!",
+            ...(!$webAppData->getRawData()['with_webview'] ? [] : [
+                'reply_markup' => InlineKeyboard::make()->setKeyboard([
+                    [InlineKeyboardButton::make('Open WebApp')->setWebApp($_ENV['RESOURCE_BASE_URL'])]
+                ])
+            ])
+        ]);
+
+        Response::send(StatusCode::OK);
+    }
+
+    private function handleRequestByDataType(WebAppData $webAppData): \Generator
+    {
         $dataType = json_decode($webAppData->getRawData()['order_data'], true)[0]["name"];
+        if (isset(self::METHOD_MAP[$dataType])) {
+            $numRequired = self::METHOD_MAP[$dataType];
 
-        if (isset($methodMap[$dataType])) {
-            header('Content-Type: application/json');
-            $numRequired = $methodMap[$dataType];
-
-             Request::sendMessage([
+            yield Request::sendMessage([
                 'chat_id' => $webAppData->getUser()->getId(),
                 'parse_mode' => ParseMode::MARKDOWN,
-                'text' => "Please type " . ($numRequired == 1 ? "1 number" : "$numRequired distinct numbers") . " between 1 and 57 (separated by spaces or commas) and then press the CONFIRM button below.",
+                'text' => sprintf(
+                    "Please type %s between 1 and 57 (separated by spaces or commas) and then press the CONFIRM button below.",
+                    $numRequired === 1 ? "1 number" : "$numRequired distinct numbers"
+                ),
                 'reply_markup' => InlineKeyboard::make()->setKeyboard([
                     [InlineKeyboardButton::make('CONFIRM')->setCallbackData("confirm")]
                 ])
@@ -62,47 +112,10 @@ class WebService extends \TelegramBot\Plugin
 
             Response::send(StatusCode::OK);
         }
-
-        if ($webAppData->getRawData()['method'] == "makeOrder") {
-            header('Content-Type: application/json');
-
-            yield Request::sendMessage([
-                'chat_id' => $webAppData->getUser()->getId(),
-                'parse_mode' => ParseMode::MARKDOWN,
-                'text' => "Your order has been placed successfully! 🍟" . "\n\n" .
-                    "Your order is: \n`" . $this->parseOrder($webAppData->getRawData()['order_data']) . "`" . "\n" .
-                    "Your order will be delivered to you in 30 minutes. 🚚",
-            ]);
-
-            Response::send(StatusCode::OK);
-        }
-
-        if ($webAppData->getRawData()['method'] == "checkInitData") {
-            header('Content-Type: application/json');
-            Response::send(StatusCode::OK);
-        }
-
-        if ($webAppData->getRawData()['method'] == "sendMessage") {
-            header('Content-Type: application/json');
-
-            yield Request::sendMessage([
-                'chat_id' => $webAppData->getUser()->getId(),
-                'parse_mode' => ParseMode::MARKDOWN,
-                'text' => "Hello World!",
-                ...(!$webAppData->getRawData()['with_webview'] ? [] : [
-                    'reply_markup' => InlineKeyboard::make()->setKeyboard([
-                        [
-                            InlineKeyboardButton::make('Open WebApp')->setWebApp($_ENV['RESOURCE_BASE_URL']),
-                        ]
-                    ])
-                ])
-            ]);
-
-            Response::send(StatusCode::OK);
-        }
     }
 
-    protected function extractNumbers($inputString) {
+    protected function extractNumbers(string $inputString): string
+    {
         preg_match_all('/\d+/', $inputString, $matches);
         return implode('', $matches[0]);
     }
