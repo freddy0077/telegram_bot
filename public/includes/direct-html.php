0486f77987f5500dbb2b72f2a6b7b9501de80cf9
Frederick <frederickankamah988@gmail.com>
2023-08-03 01:04:30 +0000

    new changes

--- public/includes/direct-html.php
+++ public/includes/direct-html.php
@@ -6,7 +6,7 @@ $data = json_decode($response, true);
 
 <section class="cafe-page cafe-items">
     <?php
-    foreach ($data['data'] as $item) {
+    foreach ($data['data'] as $item => $key) {
         // Check if game_type_id is 1 and name contains "direct"
         if (str_contains(strtolower($item['name']), 'direct')) {
 
@@ -15,7 +15,7 @@ $data = json_decode($response, true);
             $endTime = date('Y-m-d H:i:s', $item['end_time'] / 1000);
             $drawTime = date('Y-m-d H:i:s', $item['draw_time'] / 1000);
 
-            echo '<div class="cafe-item js-item" data-item-id="' . $item['game_id'] . '" data-item-name="' . $item['name'] . '" data-item-price="' . $item['min_price'] . '">';
+            echo '<div class="cafe-item js-item" data-item-id="' . $item['game_id'] . '" data-item-name="direct' . $key+1 . '" data-item-price="' . $item['min_price'] . '">';
             echo '<div class="cafe-item-counter js-item-counter">1</div>';
             echo '<div class="cafe-item-photo">';
             echo '<picture class="cafe-item-lottie js-item-lottie">
