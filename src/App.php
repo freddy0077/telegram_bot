352d906d58fab9cc69614676db4ded034a2d18e1
Frederick <frederickankamah988@gmail.com>
2023-08-03 11:09:49 +0000

    new changes

--- src/App.php
+++ src/App.php
@@ -28,6 +28,7 @@ class App extends \TelegramBot\UpdateHandler {
             $chatId = $callbackQuery->getMessage()->getChat()->getId();
             $callbackData = $callbackQuery->getData();
 
+
             if (in_array($callbackQuery->getData(), ['mtn', 'vodafone', 'airteltigo'])) {
                  Request::sendMessage([
                     'chat_id' =>  $callbackQuery->getMessage()->getChat()->getId(),
@@ -60,6 +61,13 @@ class App extends \TelegramBot\UpdateHandler {
             }
 
             if ($callbackQuery->getData()!== null && str_contains($callbackQuery->getData(), "confirm")) {
+                Request::sendMessage([
+                    'chat_id' => $callbackQuery->getMessage()->getChat()->getId(),
+                    'reply_markup' => InlineKeyboard::make()->setKeyboard([
+                        [InlineKeyboardButton::make('PAY')->setCallbackData("pay-".$callbackQuery->getData())]
+                    ])
+                ]);
+                
                 if (str_contains($callbackQuery->getData(), "megajackpot")){
                     $total = $this->extractNumbers($callbackQuery->getData());
                     Request::sendMessage([
