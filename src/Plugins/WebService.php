2dec405d8494aca8916b4eb8bb1a927077bb70aa
Frederick <frederickankamah988@gmail.com>
2023-08-03 15:17:25 +0000

    new changes

--- src/Plugins/WebService.php
+++ src/Plugins/WebService.php
@@ -22,28 +22,28 @@ use Utilities\Routing\Utils\StatusCode;
 class WebService extends \TelegramBot\Plugin
 {
 
+    private const  METHOD_MAP = [
+        "megajackpot5" => 6,
+        "megajackpot10" => 6,
+        "megajackpot20" => 6,
+        "direct1" => 1,
+        "direct2" => 2,
+        "direct3" => 3,
+        "direct4" => 4,
+        "direct5" => 5,
+        "direct6" => 6,
+        "perm2" => 3,
+        "perm4" => 4,
+        "perm5" => 5,
+        "perm6" => 6,
+    ];
+
     /**
      * @param WebAppData $webAppData
      * @return \Generator
      */
     public function onWebAppData(WebAppData $webAppData): \Generator
     {
-
-        $methodMap = [
-            "megajackpot5" => 6,
-            "megajackpot10" => 6,
-            "megajackpot20" => 6,
-            "direct1" => 1,
-            "direct2" => 2,
-            "direct3" => 3,
-            "direct4" => 4,
-            "direct5" => 5,
-            "direct6" => 6,
-            "perm2" => 3,
-            "perm4" => 4,
-            "perm5" => 5,
-            "perm6" => 6,
-        ];
 
         $dataType = json_decode($webAppData->getRawData()['order_data'], true)[0]["name"];
 
