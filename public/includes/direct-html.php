7fef2e386d53f2791e7ef9b2177bfaa6dd57b962
Frederick <frederickankamah988@gmail.com>
2023-08-03 01:09:07 +0000

    new changes

--- public/includes/direct-html.php
+++ public/includes/direct-html.php
@@ -16,27 +16,39 @@ $data = json_decode($response, true);
             $endTime = date('Y-m-d H:i:s', $item['end_time'] / 1000);
             $drawTime = date('Y-m-d H:i:s', $item['draw_time'] / 1000);
 
-            echo '<div class="cafe-item js-item" data-item-id="' . $item['game_id'] . '" data-item-name="direct' . $number+1 . '" data-item-price="' . $item['min_price'] . '">';
+            $itemAttributes = [
+                'data-item-id' => $item['game_id'],
+                'data-item-name' => "direct" . ($number + 1),
+                'data-item-price' => $item['min_price']
+            ];
+
+            $itemDataAttributes = implode(' ', array_map(function ($key, $value) {
+                return "{$key}=\"{$value}\"";
+            }, array_keys($itemAttributes), $itemAttributes));
+
+            echo "<div class=\"cafe-item js-item\" $itemDataAttributes>";
             echo '<div class="cafe-item-counter js-item-counter">1</div>';
             echo '<div class="cafe-item-photo">';
             echo '<picture class="cafe-item-lottie js-item-lottie">
                   <source type="application/x-tgsticker" srcset="../img/rsz_lottery-circle.png">
-                    <img src="../img/rsz_lottery-circle.png" alt="Burger Picture">
-                </picture> ';
+                  <img src="../img/rsz_lottery-circle.png" alt="Burger Picture">
+                  </picture>';
             echo '</div>';
             echo '<div class="cafe-item-label">';
             echo '<span class="cafe-item-title">' . $item['name'] . '</span>';
             echo '</div>';
             echo '<div class="cafe-item-buttons">';
-            echo '<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">';
-            echo '<span class="ripple-mask"><span class="ripple"></span></span>';
-            echo '</button>';
-            echo '<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">';
-            echo '<span class="button-item-label">Play</span>';
-            echo '<span class="ripple-mask"><span class="ripple"></span></span>';
-            echo '</button>';
+            echo '<button class="cafe-item-decr-button js-item-decr-btn button-item ripple-handler">
+                  <span class="ripple-mask"><span class="ripple"></span></span>
+                  </button>';
+            echo '<button class="cafe-item-incr-button js-item-incr-btn button-item ripple-handler">
+                  <span class="button-item-label">Play</span>
+                  <span class="ripple-mask"><span class="ripple"></span></span>
+                  </button>';
             echo '</div>';
             echo '</div>';
+
+            $number++;
         }
     }
     ?>
